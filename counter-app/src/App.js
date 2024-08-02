64ff71f5408e613dfe258f1f06a58101166c81a0
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-08-02 13:26:12 +0530

    Making a counter application using React State

---
+++ counter-app/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+  handleIncrement = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+  handleDecrement = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
+  handleReset = () => {
+    this.setState({
+      counter: 0,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <center>
+          <h1>Let's build a counter App</h1>
+
+          <h2>{this.state.counter}</h2>
+          <button onClick={this.handleIncrement}>Increment</button>
+          <button onClick={this.handleDecrement}>Decrement</button>
+          <button onClick={this.handleReset}>Reset</button>
+        </center>
+      </>
+    );
+  }
+}
+
+export default App;

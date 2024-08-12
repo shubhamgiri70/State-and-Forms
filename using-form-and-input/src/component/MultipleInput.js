3a7628622be08eab8161b959990ebd6c56b39bcf
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-08-12 23:19:06 +0530

    practice more form and input

--- using-form-and-input/src/component/MultipleInput.js
+++ using-form-and-input/src/component/MultipleInput.js
@@ -4,44 +4,74 @@ class MultipleInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
+      email: "",
       password: "",
+      errors: {
+        email: "",
+        password: "",
+      },
     };
   }
 
-  handleInput = (event) => {
-    let { name, value } = event.target;
-
-    this.setState({ [name]: value });
+  isValidEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
   };
 
-  handleSubmit = () => {
-    alert(this.state.username + this.state.password);
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    const errors = this.state.errors;
+
+    switch (name) {
+      case "email":
+        errors.email = this.isValidEmail(value) ? "" : "Email is not valid";
+        break;
+      case "password":
+        errors.password =
+          value.length < 6 ? "Password must be at least 6 characters" : "";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ errors, [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Your Email is: ${this.state.email} 
+    Your Password is: ${this.state.password}
+    `);
   };
 
   render() {
+    const { email, password } = this.state.errors;
+
     return (
       <>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="username">Username</label>
+          <label htmlFor="email">Email</label>
           <input
-            value={this.state.username}
-            name="username"
-            type="text"
-            placeholder="enter your username"
-            id="username"
+            value={this.state.email}
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            className={email ? "error" : ""}
             onChange={this.handleInput}
           />
+          <span>{email}</span>
+
           <label htmlFor="password">Password</label>
           <input
-            value={this.state.passowd}
+            value={this.state.password}
             type="password"
-            placeholder="please inter the password"
-            Password
-            id="password"
+            placeholder="Enter your password"
+            className={password ? "error" : ""}
             name="password"
             onChange={this.handleInput}
           />
+          <span>{password}</span>
+
           <input type="submit" id="submit" />
         </form>
       </>

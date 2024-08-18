6c473b2bb42f4527d5f6c5dd0bd4bac860774c6c
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-08-18 22:15:39 +0530

    added some functionality

--- shopping cart/src/components/Sidebar.js
+++ shopping cart/src/components/Sidebar.js
@@ -1,19 +1,20 @@
 import React from "react";
 
-function Sidebar() {
+function Sidebar({ products }) {
+  let sizes = products.reduce((acc, cv) => {
+    acc = acc.concat(cv.availableSizes);
+    return acc;
+  }, []);
+
+  let newSizes = [...new Set(sizes)];
+
   return (
     <>
       <aside className="flex-20">
         <div className="flex wrap">
-          <span>XL</span>
-          <span>XL</span>
-          <span>XL</span>
-          <span>XL</span>
-          <span>XL</span>
-          <span>XL</span>
-          <span>XL</span>
-          <span>XL</span>
-          <span>XL</span>
+          {newSizes.map((size) => {
+            return <span>{size}</span>;
+          })}
         </div>
       </aside>
     </>

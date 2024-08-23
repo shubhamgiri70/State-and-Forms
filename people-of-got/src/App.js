0d24b8d7abb215a0938d92992b916e516e9dfdc2
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-08-23 23:03:25 +0530

    added routing in got app

--- people-of-got/src/App.js
+++ people-of-got/src/App.js
@@ -2,15 +2,48 @@ import React from "react";
 import Header from "./components/Header";
 import Tags from "./components/Tags";
 import Footer from "./components/Footer";
+import data from "../src/got.json";
+import People from "../src/components/People.js";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Tags />
-      <Footer />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeHouse: "",
+    };
+  }
+
+  handleClick = (house) => {
+    this.setState({
+      activeHouse: house,
+    });
+  };
+
+  render() {
+    const houses = data.houses || [];
+    let everyone;
+    if (!this.state.activeHouse) {
+      everyone = houses.reduce((acc, house) => {
+        return acc.concat(house.people);
+      }, []);
+    } else {
+      const selectedHouse = houses.find(
+        (house) => house.name === this.state.activeHouse
+      );
+      everyone = selectedHouse ? selectedHouse.people : [];
+    }
+    return (
+      <>
+        <Header />
+        <Tags
+          activeHouse={this.state.activeHouse}
+          handleClick={this.handleClick}
+        />
+        <People everyone={everyone} activeHouse={this.props.activeHouse} />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
--- people-of-got/src/components/Tags.js
+++ people-of-got/src/components/Tags.js
@@ -1,53 +1,24 @@
 import React from "react";
 import data from "../got.json";
-import People from "./People";
 
-class Tags extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeHouse: "",
-    };
-  }
-
-  handleClick = (house) => {
-    this.setState({
-      activeHouse: house,
-    });
-  };
-
-  render() {
-    const houses = data.houses || [];
-    let houseNames = houses.map((house) => house.name);
-    let everyone;
-    if (!this.state.activeHouse) {
-      everyone = houses.reduce((acc, house) => {
-        return acc.concat(house.people);
-      }, []);
-    } else {
-      const selectedHouse = houses.find(
-        (house) => house.name === this.state.activeHouse
-      );
-      everyone = selectedHouse ? selectedHouse.people : [];
-    }
-
-    return (
-      <main>
-        <ul className="tags">
-          {houseNames.map((house) => (
-            <li
-              key={house}
-              className={house === this.state.activeHouse ? "active" : ""}
-              onClick={() => this.handleClick(house)}
-            >
-              {house}
-            </li>
-          ))}
-        </ul>
-        <People everyone={everyone} activeHouse={this.state.activeHouse} />
-      </main>
-    );
-  }
+function Tags(props) {
+  const houses = data.houses || [];
+  let houseNames = houses.map((house) => house.name);
+  return (
+    <main>
+      <ul className="tags">
+        {houseNames.map((house) => (
+          <li
+            key={house}
+            className={house === props.activeHouse ? "active" : ""}
+            onClick={() => props.handleClick(house)}
+          >
+            {house}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default Tags;
